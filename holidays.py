98ce2f76e2daebfef3826754bfb0ca8bd17380f7
Harshad Lambate <56426378+HarshadLambate@users.noreply.github.com>
2025-09-05 10:07:24 +0530

    Update holidays.py

--- holidays.py
+++ holidays.py
@@ -1,13 +1,14 @@
 from datetime import date
 # date format Y,M,D
 FESTIVALS = {
-    date(2025, 9, 5): "On occasion of Onam tomorrow is a holiday! for Hydrabad, Banglore and Chennai.",
+    date(2025, 9, 6): "On occasion of Onam tomorrow is a holiday! for Hydrabad, Banglore and Chennai.",
     date(2025, 9, 3): "On occasion of Charity day tomorrow is a holiday! for Hydrabad, Banglore and Chennai.",
     date(2025, 9, 4): "On occasion of Birthday tomorrow is a holiday! for Hydrabad, Banglore and Chennai.",
     date(2025, 10, 20): "On occasion of Diwali tomorrow is a holiday!",
     date(2025, 12, 24): "On occasion of Christmas tomorrow is a holiday!",
     date(2026, 1, 25): "On occasion of Republic Day tomorrow is a holiday!",
 }
+
 
 
 
